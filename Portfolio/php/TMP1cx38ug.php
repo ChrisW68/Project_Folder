5ea2bfa9626d99673a12b072cb03ad1d969af9bf
Chris <chris.wiseman74@gmail.com>
2016-09-20 22:07:12 -0700

    Portfolio Project

---
+++ Portfolio/php/TMP1cx38ug.php
@@ -0,0 +1,10 @@
+<link href="../css/main.css" rel="stylesheet" type="text/css">
+
+<figcaption>
+  <h3><b>Here is some of my work!</b></h3>
+  <ul>
+  	<li><a href="https://github.com/ChrisW68/LIVE-PROJECT-TECH-ACADEMY-WEBSITE">Live Project</a></li>
+    <li><a href="https://github.com/ChrisW68/Project_Folder/tree/master/AJAX%20%26%20PHP%20Chat%20messenger">Instant Messenger!</a></li>
+  </ul>
+  </p>
+</figcaption>
