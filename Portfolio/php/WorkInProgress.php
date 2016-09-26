9d55644ef8ec06e7b122b353c22731a6b4583348
Chris <chris.wiseman74@gmail.com>
2016-09-25 21:18:00 -0700

    Portfolio Updates

--- Portfolio/php/WorkInProgress.php
+++ Portfolio/php/WorkInProgress.php
@@ -1,8 +1,23 @@
-
-<h3 class="portfolio-header">Work in progress</h3>
-	<ul>
-    	<li align="left">Develop web-based application to create a auto scheduling for staff</li>
-        <li align="left">Develop web-based hiking database with gps coordinates and maps</a> 
+<a href="#"  data-toggle="modal" data-target="#modal7" class="btn btn-default" role="button"><img src="img/workinprogress-image.jpg" height="175px" alt="workInProgressLogo"/></a>
+<div id="modal7" class="modal fade" tabindex="-1" role="dialog" area-label="modalheading7">
+  <div class="modal-dialog modal-md" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span area-hidden="true">&times;</span></button>
+        <h3 class="modal-title" id="modalheading7">Modal Pop-up Title</h3>
+      </div>
+      <div class="modal-body">
+        <h3 class="portfolio-header">Work in progress</h3>
+            <ul>
+                <li align="left">Develop web-based application to create a auto scheduling for staff</li>
+                <li align="left">Develop web-based hiking database with gps coordinates and maps</a> 
+      </div>
+      <div class="modal-footer" data-dismiss="modal">
+        <button class="btn btn-primary">Cancel</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 
 
