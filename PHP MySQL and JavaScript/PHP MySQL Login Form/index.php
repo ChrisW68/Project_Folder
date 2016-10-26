1b1879ca95a2d3973dae4e41ad505b627f46a98b
Chris W <chris.wiseman74@gmail.com>
2016-10-25 21:39:56 -0700

    PHP MySQL login page additions

--- PHP MySQL and JavaScript/PHP MySQL Login Form/index.php
+++ PHP MySQL and JavaScript/PHP MySQL Login Form/index.php
@@ -10,39 +10,54 @@ require('admin/includes/pdocon.php');
 $db = new Pdocon;
 
 //Collect and process Data on login submission
-
+//If the submit_login is set then get the username and password values
 if(isset($_POST['submit_login'])){
 
-    $raw_username = cleandata($_POST['username']);
-    $raw_gender = cleandata($_POST['gender']);
-    $raw_email = cleandata($_POST['email']);  
+    $raw_email= cleandata($_POST['email']);
     $raw_password = cleandata($_POST['password']); 
 
-    $clean_username = sanitize($raw_username);
-    $clean_gender = sanitize($raw_gender);
     $clean_email = valemail($raw_email);
-    $clean_password = sanitize($raw_password);
+    $hashed_password = hashpassword($raw_password);
+    
+    $db->query('SELECT * FROM admin WHERE email=:email AND password=:password');
+    
+    $db->bindValue(':email', $clean_email, PDO::PARAM_STR);
+    $db->bindValue(':password', $hashed_password, PDO::PARAM_STR);
+    
+    $row = $db->fetchSingle();
+    
+    echo $row['email'];
+    
+    if($row){
+ /*       $_SESSION['user_data']= array (
+                                'email' = $row['email'];
+                                'id' = $row['id'];
+        
+        
+        )   */
+        
+    }else{
+            
+            echo '<div class = "alert alert-danger text-center">
+              <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+              <strong>Bummer!</strong>Password could not be found! Try Again.</div>';
+        }
 
-    $hash_password = hashpassword($clean_password);
 }
 
-
-     
-
-   
 
 ?>
   
   <div class="row">
       <div class="col-md-4 col-md-offset-4">
-          <p class=""><a class="pull-right" href="admin/register_admin.php"> Register</a></p><br>
+          <p class=""><a class="pull-left" href="admin/register_admin.php"> <bold>New User</bold></a></p><br>
       </div>
       <div class="col-md-4 col-md-offset-4">
         <form class="form-horizontal" role="form" method="post" action="index.php">
           <div class="form-group">
             <label class="control-label col-sm-2" for="email"></label>
             <div class="col-sm-10">
-              <input type="text" name="username" class="form-control" id="username" placeholder="Your name" required>
+              <input type="email" name="email" class="form-control" id="email" placeholder="Enter email" required>
             </div>
           </div>
           <div class="form-group">
