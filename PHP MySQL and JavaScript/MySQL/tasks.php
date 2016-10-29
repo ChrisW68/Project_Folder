d05bf99c41902aeb0519c6f0843b25ff61241c19
Chris W <chris.wiseman74@gmail.com>
2016-10-28 22:36:29 -0700

    Delete tasks.php

--- PHP MySQL and JavaScript/MySQL/tasks.php
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS tasks (
-    task_id     INT AUTO_INCREMENT PRIMARY KEY,
-    subject     VARCHAR (255)        DEFAULT NULL,
-    start_date  DATE                 DEFAULT NULL,
-    end_date    DATE                 DEFAULT NULL,
-    description VARCHAR (400)        DEFAULT NULL
-);
