2495455621fe81055d3a5bdf54171f767de04f85
Chris W <chris.wiseman74@gmail.com>
2016-11-07 14:48:08 -0800

    corrected some issues with the form

--- PHP MySQL and JavaScript/PHP MySQL Form/includes/electric.php
+++ PHP MySQL and JavaScript/PHP MySQL Form/includes/electric.php
@@ -2,12 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Chevy Volt Calculator</title>
+    <title>Chevy Electric Bill Calculator</title>
 </head>
 <body>
 <?php # chevyVoltCalculator.php
     
-    $page_title = "Chevy Volt Calculator";
+    $page_title = "Chevy Electric Bill Calculator";
   
     
     //Check for form submission
@@ -19,32 +19,24 @@
                  $_POST['avgTemp'], 
                  $_POST['avgkWhDaily'],
                  $_POST['avgCostDaily'],
-                 $_POST['chargeTemp'],
                  $_POST['serviceDays']) &&
                  is_numeric($_POST['electricBillMonthly']) &&
                  is_numeric($_POST['avgTemp']) &&
                  is_numeric($_POST['avgkWhDaily']) &&
                  is_numeric($_POST['avgCostDaily']) &&
-                 is_numeric($_POST['serviceDays']) &&
-                 is_numeric($_POST['chargeTemp'])) {
+                 is_numeric($_POST['serviceDays'])) {
             
         
             
          //Calculates the results
-            $voltFullyCharged = 14.4;
-            $voltDailyCommute = 9.8 + 9.8 + 5;
-            $avgVoltRange = $_POST['chargeTemp'];
-            $voltKwDailyUsed = ($voltDailyCommute / $avgVoltRange) * $voltFullyCharged;
-            $avgCostkW = $_POST['avgCostDaily'] / $_POST['avgkWhDaily'];
-            $avgDailyCost = number_format((float)($voltKwDailyUsed * $avgCostkW),2,'.', '');
-            $avgMonthly = number_format((float)(($_POST['serviceDays']*.71) * $avgDailyCost), 2, '.', '');
-            $leslieBill = number_format((float)((($_POST['electricBillMonthly'] - $avgMonthly) / 2) + $avgMonthly), 2,'.', '');
-            
+            $perkWh = number_format((float)($_POST['avgCostDaily'] / $_POST['avgkWhDaily']), 3, '.', '');
+            $monthlyCost = $_POST['electricBillMonthly'];
+     
             
         //Print the results
         echo '<h1>Total Estimated Cost</h1>
-             <p>The total cost per day of the Chevy Volt is: $' . $avgDailyCost . ' daily.<br>With a monthly
-             cost of charging the Volt being: $' . $avgMonthly . '. <br>Which will make Leslies portion: $' . $leslieBill . '<p>';
+             <p>Cost per kiloWatts is: $' . $perkWh . ' daily.<br>With a monthly
+             cost of charging the Volt being: $' . $monthlyCost . '<p>';
              
             
         } else {
@@ -57,7 +49,7 @@
  ?>
    
 <h1>Electric Bill</h1>
-   <form action="chevyVoltCalculator.php" method="post">
+   <form action="electric.php" method="post">
       <p>Electric Bill Cost: <input type="text" name="electricBillMonthly" /></p>
       <p>Avg Daily Temp: <input type="text" name="avgTemp" /></p>
       <p>Avg kWh per day: <input type="text" name="avgkWhDaily" /></p>
