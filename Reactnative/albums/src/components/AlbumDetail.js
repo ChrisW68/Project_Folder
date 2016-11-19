d6e65bdb833cc4b302562ffb39880ba1a952c063
Chris W <chris.wiseman74@gmail.com>
2016-11-19 13:20:36 -0800

    Reactnative project additional style and functionality

--- Reactnative/albums/src/components/AlbumDetail.js
+++ Reactnative/albums/src/components/AlbumDetail.js
@@ -5,26 +5,40 @@ import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
 	//Destructoring from props to reduce reference for props and albums
-	const { title, artist, thumbnail_image } = album;
+	const { title, 
+					artist, 
+					thumbnail_image, 
+					image 
+				} = album;
 	//Destructoring from styles to since it is referenced multiple times
-	const { thumbnailStyle, headerContentStyle } = styles;
+	const { thumbnailStyle, 
+					headerContentStyle,
+					thumbnailContainerStyle,
+					headerTextStyle,
+					imageStyle
+				} = styles;
 	return (
 		<Card>
 			<CardSection>
-				<View>
+				<View style={thumbnailContainerStyle}>
 					<Image 
 						style={thumbnailStyle}
 						source={{ uri: thumbnail_image }}
 					/>
 				</View>
 				<View style={headerContentStyle}>
-					<Text>
+					<Text style={headerTextStyle}>
 						{title}
 					</Text>
 					<Text>
 						{artist}
 					</Text>
 				</View>
+			</CardSection>
+			<CardSection>
+				<Image 
+					style={imageStyle}
+					source={{ uri: image }} />
 			</CardSection>
 		</Card>
 	);
@@ -35,9 +49,23 @@ const styles = {
 		flexDirection: 'column',
 		justifyContent: 'space-around'
 	},
+	headerTextStyle: {
+		fontSize: 18
+	},
 	thumbnailStyle: {
 		height: 50,
 		width: 50
+	},
+	thumbnailContainerStyle: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginLeft: 10,
+		marginRight: 10
+	},
+	imageStyle: {
+		height: 300,
+		flex: 1,
+		width: null
 	}
 };
 
