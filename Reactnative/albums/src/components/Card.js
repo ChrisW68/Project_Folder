95d9e6b99ea825ef8823d5a0fcc1069fcfa3e5d6
Chris W <chris.wiseman74@gmail.com>
2016-11-18 21:08:32 -0800

    React Project added a CardSection

--- Reactnative/albums/src/components/Card.js
+++ Reactnative/albums/src/components/Card.js
@@ -5,8 +5,7 @@ import { View } from 'react-native';
 const Card = (props) => {
 	return (
 		<View style={styles.containerStyle}>
-			{props.children}
-		</View>
+			{props.children}</View>
 	);
 };
 
@@ -19,7 +18,7 @@ const styles = {
 		shadowColor: '#000',
 		shadowOffset: { width: 0, height: 2 },
 		shadowOpacity: 0.1,
-		shadowRadius 2,
+		shadowRadius: 2,
 		elevation: 1,
 		marginLeft: 5,
 		marginRight: 5,
