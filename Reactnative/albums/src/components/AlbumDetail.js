7a0b29be534ffec9330c82311294550dad2b820a
Chris W <chris.wiseman74@gmail.com>
2016-11-19 12:57:31 -0800

    Reactnative Project adding styles

--- Reactnative/albums/src/components/AlbumDetail.js
+++ Reactnative/albums/src/components/AlbumDetail.js
@@ -1,18 +1,44 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+	//Destructoring from props to reduce reference for props and albums
+	const { title, artist, thumbnail_image } = album;
+	//Destructoring from styles to since it is referenced multiple times
+	const { thumbnailStyle, headerContentStyle } = styles;
 	return (
 		<Card>
 			<CardSection>
-				<Text>
-					{props.album.title}
-				</Text>
+				<View>
+					<Image 
+						style={thumbnailStyle}
+						source={{ uri: thumbnail_image }}
+					/>
+				</View>
+				<View style={headerContentStyle}>
+					<Text>
+						{title}
+					</Text>
+					<Text>
+						{artist}
+					</Text>
+				</View>
 			</CardSection>
 		</Card>
 	);
+};
+
+const styles = {
+	headerContentStyle: {
+		flexDirection: 'column',
+		justifyContent: 'space-around'
+	},
+	thumbnailStyle: {
+		height: 50,
+		width: 50
+	}
 };
 
 export default AlbumDetail;
