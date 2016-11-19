1cf3d31933266a290724274ec203afe450567a16
Chris W <chris.wiseman74@gmail.com>
2016-11-18 17:07:30 -0800

    Reactnative project added AlbumDetails

--- Reactnative/albums/index.android.js
+++ Reactnative/albums/index.android.js
@@ -2,20 +2,23 @@
 
 // Import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 
 //Import the relative path for the header.js
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 
 // Create a component.  Is a JavaScript function that return some amount of JSX
 // React-native take the component and render to screen
 //Component nesting.  Taking the header component and placing it in App component
 const App = () => (
-
-	//When Header is rendered the prop of headerText is passed in with a value Albums
+  //One top level tag View with two components of headerText and AlbumList
+  <View>
     <Header headerText={'Albums'} />
-    );
+    <AlbumList />
+  </View>
+);
 
 // Render it to the device
 
--- Reactnative/albums/src/components/AlbumList.js
+++ Reactnative/albums/src/components/AlbumList.js
@@ -1,20 +1,32 @@
 //Import React and also want the Component property from the React library as well
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from'./AlbumDetail';
 
 /* By using extends Component indicating 
 I want to borrow functionality from the base Component class */
 class AlbumList extends Component {
+	//Class level property
+	//Empty state 
+	state = { albums: [] };
+
 	componentWillMount() {
 		axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-		.then(response => console.log(response));
+		.then(response => this.setState({ albums: response.data }));
+	}
+
+	renderAlbums() {
+		return this.state.albums.map(album => 
+			<AlbumDetail key={album.title} album={album} />
+		);
 	}
 
 	render() {
+		console.log(this.state);
 		return (
 			<View>
-				<Text>Album List!!</Text>
+				{this.renderAlbums()}
 			</View>
 		);
 	}
