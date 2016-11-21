437b9757d474a72b29f8dc97190a2ef2082e43d5
Chris W <chris.wiseman74@gmail.com>
2016-11-20 16:01:41 -0800

    Reactnative authorization login form

--- Reactnative/auth/src/app.js
+++ Reactnative/auth/src/app.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-mport { View, Text } form 'react-native';
+import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/commons';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+	componentWillMount() {
+		firebase.initializeApp( {
+	    apiKey:'AIzaSyCjbvyj3yqh6_WiazYjspIDKGG77Ll8KJY',
+	    authDomain: 'authentication-c7a45.firebaseapp.com',
+	    databaseURL: 'https://authentication-c7a45.firebaseio.com',
+	    storageBucket: 'authentication-c7a45.appspot.com',
+	    messagingSenderId: '366396255654'
+  	});
+	}
 	render() {
 		return (
 			<View>
-				<Text>An App!</Text>
-			</View
+				<Header headerText="Authentication" />
+				<LoginForm />
+			</View>
 		);
 	}
 }
