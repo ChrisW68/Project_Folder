50e85a8116eee3e6933722143ee365338189d983
Chris W <chris.wiseman74@gmail.com>
2016-11-20 16:28:21 -0800

    Added a Text field for username

--- Reactnative/auth/src/components/LoginForm.js
+++ Reactnative/auth/src/components/LoginForm.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, TextInput } from 'react-native';
 import { Button, Card, CardSection } from './commons';
 
 class LoginForm extends Component {
 	render() {
 		return (
 			<Card>
+				//Text input for email address
+				<CardSection>
+					<TextInput style={{ height:20, width:100 }}>
+						<Text>
+						email
+						</Text>
+					</TextInput>
+				</CardSection>
+
 				<CardSection />
-				<CardSection />
+
 				<CardSection>
 					<Button>
 						Log in, Please!
