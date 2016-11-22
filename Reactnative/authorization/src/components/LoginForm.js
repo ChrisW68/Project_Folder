a8d094bfa0b819f7bf0146406867c7bbb35067f2
Chris W <chris.wiseman74@gmail.com>
2016-11-21 22:07:22 -0800

    Reactnative to add logic for signing in or signing up

--- Reactnative/authorization/src/components/LoginForm.js
+++ Reactnative/authorization/src/components/LoginForm.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './commons';
 
 class LoginForm extends Component {
 	/* Text typed in the fields exist lives a 
 	piece of state in the state component */
-	state = { email: ''};
+	state = { email: '', password: '', error: '' };
+
+	//This will authentical the user when the Log in button is pressed using firebase
+	onButtonPress() {
+		const {email, password } = this.state;
+		firebase.auth().signInWithEmailAndPassword(email, password)  //The email and password properties are passed to the function
+			.catch(() => {
+				firebase.auth().createUserWithEmailAndPassword(email, password)
+					.catch(() => {
+						this.setState({ error: 'Uh Oh, Authentification Failed!'})
+					});
+			});
+	}
 
 	render() {
 		return (
@@ -21,10 +35,23 @@ class LoginForm extends Component {
 					/>
 				</CardSection>
 
-				<CardSection />
+				<CardSection>
+					<Input
+						secureTextEntry //By having a boolean property as true only listing the property
+						placeholder="password"
+						label="Password"
+						value={this.state.password}
+						onChangeText={password => this.setState({ password })}
+
+					/>
+				</CardSection>
+
+				<Text style={styles.errorTextStyle}>
+					{this.state.error}
+				</Text>
 
 				<CardSection>
-					<Button>
+					<Button onPress={this.onButtonPress.bind(this)}>
 						Log In
 					</Button>
 				</CardSection>
@@ -32,5 +59,13 @@ class LoginForm extends Component {
 		);
 	}
 }
+
+const styles = {
+	errorTextStyle: {
+		fontSize: 20,
+		alignSelf: 'center',
+		color: 'red'
+	}
+};
 
 export default LoginForm;
