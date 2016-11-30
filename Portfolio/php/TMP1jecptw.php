4c5ecb4b3cf15e2f4a7176a58c2d4c69a91302f2
Chris W <chris.wiseman74@gmail.com>
2016-11-29 21:36:53 -0800

    Portfolio website
    Added button to return to homepage

---
+++ Portfolio/php/TMP1jecptw.php
@@ -0,0 +1,24 @@
+<link href="../css/main.css" rel="stylesheet" type="text/css">
+<div class="overlay-3">
+              <!-- title -->
+  <div class="content-block text-center" id="portfolio" margin-top="10px">
+    <header class="block-heading cleafix">
+        <h1>A little history about me and how it's made me who I am today</h1></center>
+    </header>
+      
+    <p class="col-md" align="center">Since I was a kid, I was always curious how things worked and how to make things better.  My parents always taught me to ask Why, and could it be better?</br>
+    </p>
+    <p class="col-md" align="center">                          My dad was a drafting teacher as well a carpenter during the summer break; at an early age, my dad got me curious in engineering.  He used to do contract work, developing section/cut-away drawings for GE jet engines and from my earliest memories, I was amazed at how he could think in such a way of visualizing what the part would look like.  My dad saw something in me where I was interested in the details of how things worked.  As I grew up, my parents (especially my dad) instilled in me that if I want to be an engineer, I must consider it from every viewpoint and every part of the process is equally important. </br>
+    </p>
+          <p class="col-md" align="center">I started my career as a Manufacturing Engineer.  From the start of engineering I was pushing technology to the limits.  Asking machine builders for some extras or asking why it was not standard.  I loved getting my hands dirty and figuring out how things ticked, only allowed to understand the machine after it broke and needing to fix it quickly. I enjoy being challenged and finding solutions to problems.  During my years in engineering, I always felt if there was an app or a program for this it would make my job and the people I work with easier and better.    I would create Access databases for coworkers to better track what was being produced and give unbiased views of the strengths and weaknesses of the process. </br>
+                              </p>
+          <p class="col-md" align="center">When not engineering or programing:</br>
+                              I enjoy working on my house (always making it just a little more energy efficient and adding unique touches).  Trying (I do mean TRYING) to grow something in my vegetable garden.  When I have time, I find new places to explore hiking/cross-country/canoeing/paddle boarding. </p>
+      
+  </div>
+  <div>
+    <?php include ("php/contactme1.php");?>
+  </div> 
+  	<!-- Pulse arrow down to next section button that pulsates-->
+	<center><a href="#softskills"><i class="fa fa-angle-down fa-3x pulse" align="center"></i></a><center>
+</div><!-- overlay end -->  
