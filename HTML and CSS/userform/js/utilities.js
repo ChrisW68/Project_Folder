d202ef64d7ffbbb88dbb030e8d7b21f8ba8ff5eb
Chris W <chris.wiseman74@gmail.com>
2016-11-30 20:47:23 -0800

    Simple user login display

---
+++ HTML and CSS/userform/js/utilities.js
@@ -0,0 +1,12 @@
+// JavaScript Document: Helper function to add an event listener
+function addEvent(el, event, callback) {
+	if('addEventListener' in el) {								// If addEventListener works
+		el.addEventListener(event, callback, false);			// Use it
+	} else {													// Otherwise
+		el['e' + event + callback] = callback;					// Make callback a method of el
+		el[event + callback] = function() {						// Add second method
+			el['e' + event + callback](window.event);			// Use it to cal prev func
+		};
+		el.attachEvent('on' + event, el[event + callback]);		// Use attachEvent() 
+	}
+}
