95fe470854c01fd32c0be3f7fb49f51799f8369d
Chris W <chris.wiseman74@gmail.com>
2016-12-06 12:55:53 -0800

    Reactnative project
    Added a touchablewithoutfeedback component

--- Reactnative/tech_stack/src/components/ListItem.js
+++ Reactnative/tech_stack/src/components/ListItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View} from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
@@ -8,11 +8,19 @@ class ListItem extends Component {
 	render() {
 		//Destructor the titleStyle off the styles object
 		const { titleStyle } = styles;
+		const { id, title } = this.props.library;
+
 		return (
-			<CardSection>
-			<Text style={titleStyle}>
-				{this.props.library.title}</Text>
-			</CardSection>
+			<TouchableWithoutFeedback
+				onPress={() => this.props.selectLibrary(id)}
+			>
+				<View>
+					<CardSection>
+					<Text style={titleStyle}>
+						{title}</Text>
+					</CardSection>
+				</View>
+			</TouchableWithoutFeedback>
 		);
 	}
 }
