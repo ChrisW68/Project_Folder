8c91b839dbda6ca152957a19f067d15b2d1f1ef7
Chris W <chris.wiseman74@gmail.com>
2016-12-07 16:21:08 -0800

    Reactnative project manager scheduling

--- Reactnative/manager/src/App.js
+++ Reactnative/manager/src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { creativeStore } from 'redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
+
 
 class App extends Component {
+	componentWillMount() {
+		const config = {
+    apiKey: 'AIzaSyDz4fXwGfd_yhvsUZgZ_lfrakchu2LZfDM',
+    authDomain: 'employee-adae1.firebaseapp.com',
+    databaseURL: 'https://employee-adae1.firebaseio.com',
+    storageBucket: 'employee-adae1.appspot.com',
+    messagingSenderId: '485210606743'
+  };
+  firebase.initializeApp(config);
+	}
+
 	render() {
 		return (
-			<Provider store={creativeStore(reducers)}>
-				<View>
-					<Text>
-						Hello!
-					</Text>
-				</View>
+			<Provider store={createStore(reducers)}>
+				<LoginForm />
 			</Provider>
 		);
 	}
