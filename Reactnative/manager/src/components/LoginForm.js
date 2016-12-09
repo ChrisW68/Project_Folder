f8dc3f10f9ef6819f243c404cc4e0c75aaecbcbd
Chris W <chris.wiseman74@gmail.com>
2016-12-08 16:59:04 -0800

    Reactnative project
    Add loginform with functions for both email and password

--- Reactnative/manager/src/components/LoginForm.js
+++ Reactnative/manager/src/components/LoginForm.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { emailChanged } from '../actions';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
 	onEmailChange(text) {
 		this.props.emailChanged(text);
+	}
+
+	onPasswordChange(text) {
+		this.props.passwordChanged(text);
+	}
+
+	onButtonPress() {
+		const { email, password } = this.props;
+
+		this.props.loginUser({ email, password });
 	}
 
 	render() {
@@ -16,6 +26,7 @@ class LoginForm extends Component {
 						label="Email"
 						placeholder="email@gmail.com"
 						onChangeText={this.onEmailChange.bind(this)}
+						value={this.props.email}
 					/>
 				</CardSection>
 
@@ -24,11 +35,13 @@ class LoginForm extends Component {
 						secureTextEntry
 						label="Password"
 						placeholder="password"
+						onChangeText={this.onPasswordChange.bind(this)}
+						value={this.props.password}
 					/>
 				</CardSection>
 
 				<CardSection>
-					<Button>
+					<Button onPress={this.onButtonPress.bind(this)}>
 						Login
 					</Button>
 				</CardSection>
@@ -37,4 +50,15 @@ class LoginForm extends Component {
 	}
 }
 
-export default connect(null, { emailChanged })(LoginForm);
+const mapStateToProps = state => {
+	return {
+		email: state.auth.email,
+		password: state.auth.password
+	};
+};
+
+export default connect(mapStateToProps, { 
+	emailChanged, 
+	passwordChanged, 
+	loginUser 
+})(LoginForm);
--- Reactnative/manager/src/reducers/AuthReducer.js
+++ Reactnative/manager/src/reducers/AuthReducer.js
@@ -1,11 +1,26 @@
-import { EMAIL_CHANGED } from '../actions/types';
+import { 
+	EMAIL_CHANGED, 
+	PASSWORD_CHANGED
+	} from '../actions/types';
 
-const INITIAL_STATE = { email: '' };
+//This reducer is responsible for email and password property
+const INITIAL_STATE = { 
+	email: '',
+	password: '' 
+};
 
 export default (state = INITIAL_STATE, action) => {
 	switch (action.type) {
 		case EMAIL_CHANGED:
-			console.log('action!');
+			/* Make a new object, take all the properties from existing
+			   state object and place them in the object
+			   then define the property email and give it a value of 
+			   action dot payload and place it on top of whatever properties
+			   that were on the state object*/
+			return { ...state, email: action.payload };
+		case PASSWORD_CHANGED:
+			return { ...state, password: action.payload };
+
 		default:
 			return state;
 	}
