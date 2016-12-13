915bfefea1e775c24791db63e354b4038b67e53f
Chris W <chris.wiseman74@gmail.com>
2016-12-13 13:45:08 -0800

    Reactnative project
    Added functionality to Login Screen
    * Spinner
    * Error Message for incorrect password or email

--- Reactnative/manager/src/reducers/AuthReducer.js
+++ Reactnative/manager/src/reducers/AuthReducer.js
@@ -1,15 +1,21 @@
 import { 
 	EMAIL_CHANGED, 
-	PASSWORD_CHANGED
+	PASSWORD_CHANGED,
+	LOGIN_USER_SUCCESS,
+	LOGIN_USER_FAIL,
+	LOGIN_USER
 	} from '../actions/types';
 
 //This reducer is responsible for email and password property
 const INITIAL_STATE = { 
 	email: '',
-	password: '' 
+	password: '',
+	user: null,
+	loading: false
 };
 
 export default (state = INITIAL_STATE, action) => {
+	//console.log(action);
 	switch (action.type) {
 		case EMAIL_CHANGED:
 			/* Make a new object, take all the properties from existing
@@ -19,8 +25,22 @@ export default (state = INITIAL_STATE, action) => {
 			   that were on the state object*/
 			return { ...state, email: action.payload };
 		case PASSWORD_CHANGED:
-			return { ...state, password: action.payload };
-
+			return { ...state, 
+				password: action.payload 
+			};
+		case LOGIN_USER:
+			return { ...state, 
+				loading: true, 
+				error: '' 
+			};
+		case LOGIN_USER_SUCCESS:
+			return { ...state, ...INITIAL_STATE, user: action.payload };
+		case LOGIN_USER_FAIL:
+			return { ...state, 
+				error: 'Authentication Failed.',
+				password: '', 
+				loading: false 
+			};
 		default:
 			return state;
 	}
